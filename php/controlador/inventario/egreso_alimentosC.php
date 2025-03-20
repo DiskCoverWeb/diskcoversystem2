fbf20f1e602e4bae217f7b963e0769ebe6082ad6
javier farinango <javier.farinango92@gmail.com>
2025-03-19 19:04:05 -0500

    cambiso

--- php/controlador/inventario/egreso_alimentosC.php
+++ php/controlador/inventario/egreso_alimentosC.php
@@ -223,7 +223,7 @@ class egreso_alimentosC
 		$datos  = $this->modelo->buscar_producto_egreso();
 		if(count($datos)>0)
 		{
-			return -3
+			return -3;
 		}
 		
 		// para el cheing de egreso se colocara la G
