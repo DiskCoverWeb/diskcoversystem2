fdd75c8584f713b9e84944ef50e5b31a1c1b70a7
javier farinango <javier.farinango92@gmail.com>
2026-02-06 09:45:20 -0500

    descarga

--- php/modelo/facturacion/lista_notas_creditoM.php
+++ php/modelo/facturacion/lista_notas_creditoM.php
@@ -18,7 +18,7 @@ class lista_notas_creditoM
 
 	function notas_credito_emitidas_tabla($codigo=false,$desde=false,$hasta=false,$serie=false,$secuencia_NC=false)
 	{
-		$sql = " SELECT TOP 300 FA.Periodo,FA.Item,F.T,F.TP, F.Fecha, C.Cliente, F.Serie, F.Factura, F.Banco, F.Cheque, F.Abono, F.Mes, F.Comprobante, F.Autorizacion, F.Serie_NC,Secuencial_NC, F.Autorizacion_NC, F.Base_Imponible, F.Porc, C.Representante As Razon_Social, F.Cta,F.Cta_CxP,FA.Descuento2,C.CI_RUC,C.Email,C.Codigo,C.EmailR,C.Email2,F.Cod_Ejec,Tipo_Pago,F.CodigoU,FA.TB,FA.Total_MN,FA.Descuento,FA.Porc_IVA ,FA.Cod_CxC,FA.Fecha as FechaF   
+		$sql = " SELECT TOP 300 FA.Periodo,FA.Item,F.T,F.TP, F.Fecha, C.Cliente, F.Serie, F.Factura, F.Banco, F.Cheque, F.Abono, F.Mes, F.Comprobante, F.Autorizacion, F.Serie_NC,Secuencial_NC, F.Autorizacion_NC, F.Base_Imponible, F.Porc, C.Representante As Razon_Social, F.Cta,F.Cta_CxP,FA.Descuento2,C.CI_RUC,C.Email,C.Codigo,C.EmailR,C.Email2,F.Cod_Ejec,Tipo_Pago,F.CodigoU,FA.TB,FA.Total_MN,FA.Descuento,FA.Porc_IVA ,FA.Cod_CxC,FA.Fecha as FechaF,FA.TC,FA.IVA,FA.Nota   
            FROM Trans_Abonos As F,Clientes C,Facturas FA 
            WHERE F.Item = FA.Item
 			AND F.Periodo = FA.Periodo
@@ -46,7 +46,7 @@ class lista_notas_creditoM
 	    {
 	    	$sql.=" AND Secuencial_NC = '".$secuencia_NC."'";
 	    }
-	    $sql.=" group by FA.Periodo,FA.Item, F.T,F.TP, F.Fecha, C.Cliente, F.Serie, F.Factura, F.Banco, F.Cheque, F.Abono, F.Mes, F.Comprobante, F.Autorizacion, F.Serie_NC,Secuencial_NC,F.Autorizacion_NC,F.Base_Imponible,F.Porc,C.Representante,F.Cta,F.Cta_CxP,FA.Descuento2,C.CI_RUC,C.Email,C.Codigo,C.EmailR,C.Email2,F.Cod_Ejec,F.CodigoU,Tipo_Pago,FA.TB,FA.Total_MN,FA.Descuento,FA.Porc_IVA,FA.Cod_CxC,FA.Fecha   ";
+	    $sql.=" group by FA.Periodo,FA.Item, F.T,F.TP, F.Fecha, C.Cliente, F.Serie, F.Factura, F.Banco, F.Cheque, F.Abono, F.Mes, F.Comprobante, F.Autorizacion, F.Serie_NC,Secuencial_NC,F.Autorizacion_NC,F.Base_Imponible,F.Porc,C.Representante,F.Cta,F.Cta_CxP,FA.Descuento2,C.CI_RUC,C.Email,C.Codigo,C.EmailR,C.Email2,F.Cod_Ejec,F.CodigoU,Tipo_Pago,FA.TB,FA.Total_MN,FA.Descuento,FA.Porc_IVA,FA.Cod_CxC,FA.Fecha,FA.TC,FA.IVA,FA.Nota     ";
 	    $sql.=" ORDER BY F.Banco, F.Cheque, C.Cliente, F.Serie, F.Factura, F.Fecha ";
 		// print_r($sql);die();    
 		return $this->db->datos($sql);
