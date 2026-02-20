0a4d6bcbd5e5d878b86a1580b0e6e578b8ab06d2
javier farinango <javier.farinango92@gmail.com>
2026-02-19 19:43:42 -0500

    cambios sri visual

--- php/comprobantes/SRI/autorizar_sri_visual.php
+++ php/comprobantes/SRI/autorizar_sri_visual.php
@@ -62,8 +62,8 @@ class autoriza_sri
 		$this->iv = base64_decode("C9fBxl1EWtYTL1/M8jfstw==");
 		// $this->conn = new Conectar();
 		$this->db = new db();
-		// $this->rutaJava8  = "";
-		$this->rutaJava8  = escapeshellarg("C:\\Program Files\\Java\\jdk-1.8\\bin\\");
+		$this->rutaJava8  = "";
+		// $this->rutaJava8  = escapeshellarg("C:\\Program Files\\Java\\jdk-1.8\\bin\\");
 	}
 
 
