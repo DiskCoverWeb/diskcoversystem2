44370029f0e5465286caaa7e2c368b32fd7e1a89
javier farinango <javier.farinango92@gmail.com>
2026-08-03 09:04:37 -0500

    footer

--- headers/footer.php
+++ headers/footer.php
@@ -9,7 +9,7 @@
 		<!--Start Back To Top Button--> <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
 		<!--End Back To Top Button-->
 		<footer class="page-footer" id="footerInfo" style="font-size: x-small;">
-			<p class="mb-0"><img src="../../img/logotipos/diskcover_web.gif" class="m-1" style="width:60px"><b class="breadcrumb-title pe-1" style="font-size: 12px;"></b> <b class="m-1">DIRECCION:</b> Atacames N23-226 y Av. La Gasca - <b>EMAIL:</b> prisma_net@hotmail.com / informacion@diskcoversystem.com - <b>TELEFONO:</b> (+593)989105300 - 0999654196</p>
+			<p class="mb-0"><img src="../../img/logotipos/diskcover_web.gif" class="m-1" style="width:60px"><b class="breadcrumb-title pe-1" style="font-size: 12px;"></b> <b class="m-1">DIRECCION:</b> Atacames N23-226 y Av. La Gasca - <b>EMAIL:</b> prisma_net@hotmail.com / informacion@diskcoversystem.com - <b>TELEFONO:</b> (+593)99-965-4196 / 098-910-5300 / 099-467-8317</p>
 		</footer>
 	</div>
 	
